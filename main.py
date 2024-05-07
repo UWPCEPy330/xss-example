eb9fc54ff8a4d47346ad2e09d5c90acf86f99432
evanakin <55139730+evanakin@users.noreply.github.com>
2024-05-07 11:45:30 -0700

    commit

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import os
 import base64
+import html
+import random
 
-from flask import Flask, request
+from flask import Flask, request, session
 from model import Message 
 
 app = Flask(__name__)
+app.secret_key = '677631026d05fa8a60bd186803779dbbe3c7db0c017dd8b7'
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
 
+    # If the session does not include a CSRF token, then add one.
+    if 'csrf_token' not in session:
+        session['csrf_token'] = str(random.randint(10000000, 99999999))
+
     if request.method == 'POST':
-        m = Message(content=request.form['content'])
-        m.save()
+        if request.form.get('csrf_token') == session['csrf_token']:
+            m = Message(content=request.form['content'])
+            m.save()
 
     body = """
 <html>
@@ -20,18 +28,20 @@ def home():
 <h2>Contribute to the Knowledge of Others</h2>
 <form method="POST">
     <textarea name="content"></textarea>
+    <input type="hidden" name="csrf_token" value="{csrf_token}">
     <input type="submit" value="Submit">
 </form>
 
 <h2>Wisdom From Your Fellow Classmates</h2>
-"""
+""".format(csrf_token=session['csrf_token'])
     
     for m in Message.select():
-        body += """
+        escaped_content = html.escape(m.content)
+        body += f"""
 <div class="message">
-{}
+{escaped_content}
 </div>
-""".format(m.content.replace('<', '&lt;').replace('>', '&gt;'))
+"""
 
     return body 
 

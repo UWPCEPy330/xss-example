2bf154f02ff3fa94a38e8a62365580f2c92c1119
Luis Conejo-Alpizar <luisconej@gmail.com>
2024-06-26 22:05:21 -0700

    Update main.py
    Restoring main.py to previous version (it appears a student commit directly into the template was merged).

--- main.py
+++ main.py
@@ -1,25 +1,17 @@
 import os
 import base64
-import html
-import random
 
-from flask import Flask, request, session
+from flask import Flask, request
 from model import Message 
 
 app = Flask(__name__)
-app.secret_key = '677631026d05fa8a60bd186803779dbbe3c7db0c017dd8b7'
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
 
-    # If the session does not include a CSRF token, then add one.
-    if 'csrf_token' not in session:
-        session['csrf_token'] = str(random.randint(10000000, 99999999))
-
     if request.method == 'POST':
-        if request.form.get('csrf_token') == session['csrf_token']:
-            m = Message(content=request.form['content'])
-            m.save()
+        m = Message(content=request.form['content'])
+        m.save()
 
     body = """
 <html>
@@ -28,20 +20,18 @@ def home():
 <h2>Contribute to the Knowledge of Others</h2>
 <form method="POST">
     <textarea name="content"></textarea>
-    <input type="hidden" name="csrf_token" value="{csrf_token}">
     <input type="submit" value="Submit">
 </form>
 
 <h2>Wisdom From Your Fellow Classmates</h2>
-""".format(csrf_token=session['csrf_token'])
+"""
     
     for m in Message.select():
-        escaped_content = html.escape(m.content)
-        body += f"""
+        body += """
 <div class="message">
-{escaped_content}
+{}
 </div>
-"""
+""".format(m.content)
 
     return body 
 
@@ -49,4 +39,3 @@ def home():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
     app.run(host='0.0.0.0', port=port)
-
